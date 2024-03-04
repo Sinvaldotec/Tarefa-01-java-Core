2c13041c19cd5b25ef3516c2d194858acf7b8a0d
Sinvaldo JPS <sinvaldo.tecnologia@gmail.com>
2024-03-03 23:40:32 -0300

    Add files via upload

---
+++ Lista01Exercício02/src/exercicio02/exercicio02.java
@@ -0,0 +1,16 @@
+package exercicio02;
+
+import java.util.Scanner;
+
+public class exercicio02 {
+
+	public static void main(String[] args) {
+		System.out.println("Digite um número para que seja exibido o seu antecessor");
+
+		Scanner sc = new Scanner(System.in);
+
+		int valor=sc.nextInt();
+
+		System.out.println(valor-1);
+
+		}}
