eaee48c627d2a918694a34fbb71beb266ef8e935
Sinvaldo JPS <sinvaldo.tecnologia@gmail.com>
2024-03-04 00:39:17 -0300

    Add files via upload

---
+++ Lista01Exercício03/src/exercicio03/exercicio03.java
@@ -0,0 +1,20 @@
+package exercicio03;
+
+import java.util.Scanner;
+
+public class exercicio03 {
+
+	public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+
+System.out.println("Digite o Primeiro valor");
+
+int n1= sc.nextInt();
+
+System.out.println("Digite o Segundo valor que deseja somar");
+
+int n2= sc.nextInt();
+
+System.out.println(n1+n2);
+
+}}
---
+++ Lista01Exercício03/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Lista01Exercício03 {
+}
